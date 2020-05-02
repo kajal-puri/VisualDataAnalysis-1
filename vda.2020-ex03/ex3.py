f97b099c3abe09da024b6f99d10fc8679ab4ac71
unknown <s6anghos@uni-bonn.de>
2020-05-02 18:54:47 +0200

    ex3

---
+++ vda.2020-ex03/ex3.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  2 18:53:54 2020
+
+@author: anirbanhp
+
+Team: 
+    Anirban Ghosh
+    Kajal Puri
+    Shilpa Chatterjee
+"""
+
+
+import matplotlib.cm as cm
+import matplotlib.image as mpimg
+import matplotlib.colors as clr
+import matplotlib.pyplot as plt
+import numpy as np
+
+import seaborn as sns
+import pandas as pd
